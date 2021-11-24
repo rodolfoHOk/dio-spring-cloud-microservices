d629bced8a6575ab069746521ce6daa57b39094e
rodolfoHOk <rodolfo.okino@gmail.com>
2021-11-23 21:28:53 -0300

    add spring cloud config server and config clients

---
+++ config-server/src/test/java/me/dio/rodolfohok/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.rodolfohok.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
